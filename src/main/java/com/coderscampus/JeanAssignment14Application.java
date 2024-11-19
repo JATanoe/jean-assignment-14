60cd852796dd346b259ae460e9aaaeb61e7f8971
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-11-18 23:26:55 -0600

    Initial commit

---
+++ src/main/java/com/coderscampus/JeanAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeanAssignment14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeanAssignment14Application.class, args);
+    }
+
+}
